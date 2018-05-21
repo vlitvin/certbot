e9d4cef7e2d4a24a04235e2d0103079cff20364e
vlitvin <victor@litvin.pro>
2018-05-21 18:06:19 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+certbot certonly \
+        --standalone
+        --domains ${DOMAINS}
+        --certname ${CERTNAME}
+ 
+echo "
+kind: Secret
+apiVersion: v1
+type: Opaque
+metadata:
+  name: $CERTNAME
+  namespace: ${KUBE_NAMESPACE}
+data:
+  tls.crt:`cat /etc/letsencrypt/live/k8s-certbot/fullchain.pem | base64 -w 0`
+  tls.key:`cat /etc/letsencrypt/live/k8s-certbot/privkey.pem | base64 -w 0`
+" | kubectl apply --force -f - 
