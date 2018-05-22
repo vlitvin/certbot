76ce29f2eade7efddcc61d76a979d90f5401b684
vlitvin <victor@litvin.pro>
2018-05-22 12:24:14 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
+echo "Starting webserver"
+cd /le
+python -m SimpleHTTPServer 80
+
+echo "Waiting for k8s do its service and ingress job"
+sleep 30
+
+echo "Obtaining cert"
 certbot certonly \
-        --standalone \
+        --webroot \
+        -w /le
         --non-interactive \
         --agree-tos \
         --domains ${DOMAINS} \
         --cert-name ${CERTNAME} \
         -m ${EMAIL}
- 
+
+echo "Deploying secret"
 echo "
 kind: Secret
 apiVersion: v1
@@ -16,6 +26,6 @@ metadata:
   name: $CERTNAME
   namespace: ${KUBE_NAMESPACE}
 data:
-  tls.crt:`cat /etc/letsencrypt/live/k8s-certbot/fullchain.pem | base64 -w 0`
-  tls.key:`cat /etc/letsencrypt/live/k8s-certbot/privkey.pem | base64 -w 0`
+  tls.crt:`cat /etc/letsencrypt/live/${CERTNAME}/fullchain.pem | base64 -w 0`
+  tls.key:`cat /etc/letsencrypt/live/${CERTNAME}/privkey.pem | base64 -w 0`
 " | kubectl apply --force -f - 
