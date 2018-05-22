7e5e550e3f310be10b96fa8cf15aaf6c2b49fb17
vlitvin <victor@litvin.pro>
2018-05-22 12:44:12 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -26,6 +26,6 @@ metadata:
   name: $CERTNAME
   namespace: ${KUBE_NAMESPACE}
 data:
-  tls.crt:`cat /etc/letsencrypt/live/${CERTNAME}/fullchain.pem | base64 -w 0`
-  tls.key:`cat /etc/letsencrypt/live/${CERTNAME}/privkey.pem | base64 -w 0`
+  tls.crt: `cat /etc/letsencrypt/live/${CERTNAME}/fullchain.pem | base64 -w 0`
+  tls.key: `cat /etc/letsencrypt/live/${CERTNAME}/privkey.pem | base64 -w 0`
 " | kubectl apply --force -f - 
